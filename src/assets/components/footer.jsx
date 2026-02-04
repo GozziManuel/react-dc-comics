f6978f015fc5216aa37aee9b927fc8f1f5f27974
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-04 22:47:12 +0100

    footer

--- src/assets/components/footer.jsx
+++ src/assets/components/footer.jsx
@@ -1,7 +1,33 @@
 export default function Footer() {
   return (
     <>
-      <h1>Sono il footer</h1>
+      <div class="footerContainer">
+        <div class="container">
+          <div class="d-flex justify-content-between align-items-center">
+            <div>
+              <button class="myButton p-2">SIGN-UP NOW!</button>
+            </div>
+            <ul class="footerList">
+              <li>FOLLOW US</li>
+              <li>
+                <img src="/footer-facebook.png" alt="social" />
+              </li>
+              <li>
+                <img src="/footer-twitter.png" alt="social" />
+              </li>
+              <li>
+                <img src="/footer-youtube.png" alt="social" />
+              </li>
+              <li>
+                <img src="/footer-pinterest.png" alt="social" />
+              </li>
+              <li>
+                <img src="/footer-periscope.png" alt="social" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
