c7e86da415517c6d7b17ebae0304f4d7bc03ccab
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-04 21:44:56 +0100

    blue section

--- src/assets/components/Blue.jsx
+++ src/assets/components/Blue.jsx
@@ -1,7 +1,52 @@
 export default function BlueSection() {
   return (
     <>
-      <div class="container ">Hello World</div>
+      <div class="blueContainer">
+        <div class="container d-flex justify-content-center align-items-center">
+          <ul class="flexcontainer">
+            <li class="d-inline-block blueList">
+              <img
+                src="buy-comics-digital-comics.png"
+                alt="comics"
+                class="blueSectionImage"
+              />
+              DIGITAL COMICS
+            </li>
+            <li class="d-inline-block blueList">
+              <img
+                src="buy-comics-merchandise.png"
+                alt="merchant"
+                class="blueSectionImage"
+              />
+              DC MERCHANDISE
+            </li>
+            <li class="d-inline-block blueList">
+              <img
+                src="buy-comics-shop-locator.png"
+                alt="locator"
+                class="blueSectionImage"
+              />
+              SUBSCRIPTION
+            </li>
+            <li class="d-inline-block blueList">
+              <img
+                src="buy-comics-subscriptions.png"
+                alt="subscr"
+                class="blueSectionImage"
+              />
+              COMIC SHOP LOCATOR
+            </li>
+            <li class="d-inline-block blueList">
+              <img
+                src="buy-dc-power-visa.svg"
+                alt="power"
+                class="blueSectionImage"
+              />
+              DC POWER VISA
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
