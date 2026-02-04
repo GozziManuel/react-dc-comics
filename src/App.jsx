5cdb7a80b25571a9f68ff720245db98b2d8b2a3d
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-04 18:40:04 +0100

    added sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-function App() {
+import Header from "./assets/components/header";
+import Footer from "./assets/components/footer";
+import Main from "./assets/components/main";
+import Section from "./assets/components/section";
+
+export default function App() {
   return (
     <>
       <Header />
+      <Section />
+      <Main />
+      <Footer />
     </>
   );
 }
-
-export default App;
