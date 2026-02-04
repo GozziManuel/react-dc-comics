2f0b16ccd265e4e1ffe93b13f4aea03e1539376c
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-04 20:29:52 +0100

    finished header

--- src/assets/components/header.jsx
+++ src/assets/components/header.jsx
@@ -5,10 +5,16 @@ export default function Header() {
         <div class="d-flex justify-content-between align-items-center">
           <img src="/dc-logo.png" alt="logo" />
           <ul>
-            <li class="d-inline-block mx-4"> prova </li>
-            <li class="d-inline-block mx-4"> prova </li>
-            <li class="d-inline-block mx-4"> prova </li>
-            <li class="d-inline-block mx-4"> prova </li>
+            <li class="d-inline-block myFont"> CHARACTERS </li>
+            <li class="d-inline-block myFont"> COMICS </li>
+            <li class="d-inline-block myFont"> MOVIES </li>
+            <li class="d-inline-block myFont"> TV </li>
+            <li class="d-inline-block myFont"> GAMES </li>
+            <li class="d-inline-block myFont"> COLLECTIBLES </li>
+            <li class="d-inline-block myFont"> VIDEOS </li>
+            <li class="d-inline-block myFont"> FANS </li>
+            <li class="d-inline-block myFont"> NEWS </li>
+            <li class="d-inline-block myFont"> SHOP </li>
           </ul>
         </div>
       </div>
