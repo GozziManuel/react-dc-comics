a6b1fe797567629810a543739a2b7a33175a0050
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-04 20:00:16 +0100

    header

--- src/assets/components/header.jsx
+++ src/assets/components/header.jsx
@@ -1,3 +1,17 @@
 export default function Header() {
-  return <h1>Sono l'header</h1>;
+  return (
+    <>
+      <div className="container my-4">
+        <div class="d-flex justify-content-between align-items-center">
+          <img src="/dc-logo.png" alt="logo" />
+          <ul>
+            <li class="d-inline-block mx-4"> prova </li>
+            <li class="d-inline-block mx-4"> prova </li>
+            <li class="d-inline-block mx-4"> prova </li>
+            <li class="d-inline-block mx-4"> prova </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
