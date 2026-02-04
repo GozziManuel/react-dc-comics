c29db88382928198738718c76dd7c5055bd127aa
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-04 22:19:10 +0100

    added list and backg

--- src/assets/components/MainSection.jsx
+++ src/assets/components/MainSection.jsx
@@ -1,7 +1,65 @@
 export default function mainSection() {
   return (
     <>
-      <div class="container "></div>
+      <div class="listContainer">
+        <div class="container d-flex py-5">
+          <div>
+            <ul class="flexList">
+              <li>
+                <h2 class="mb-2 comicsTitle" Shop DC>
+                  DC COMICS
+                </h2>
+              </li>
+              <li class="comicsList">Characters</li>
+              <li class="comicsList">Comics</li>
+              <li class="comicsList">Movies</li>
+              <li class="comicsList">TV</li>
+              <li class="comicsList">Games</li>
+              <li class="comicsList">Videos</li>
+              <li class="comicsList">News</li>
+            </ul>
+            <ul class="flexList">
+              <li>
+                <h2 class="mb-2 comicsTitle">SHOP</h2>
+              </li>
+              <li class="comicsList">Shop DC</li>
+              <li class="comicsList">Shop DC Collectibles</li>
+            </ul>
+          </div>
+          <div>
+            <ul class="flexList">
+              <li>
+                <h2 class="mb-2 comicsTitle" Shop DC>
+                  DC COMICS
+                </h2>
+              </li>
+              <li class="comicsList">Terms Of Use</li>
+              <li class="comicsList">Privacy policy(New)</li>
+              <li class="comicsList">Ad Choices </li>
+              <li class="comicsList">Advertising</li>
+              <li class="comicsList">Jobs</li>
+              <li class="comicsList">Subscription</li>
+              <li class="comicsList">Talent Workshop</li>
+              <li class="comicsList">CPSC Certificates</li>
+              <li class="comicsList">Ratings</li>
+              <li class="comicsList">Shop Help</li>
+              <li class="comicsList">Contact Us</li>
+            </ul>
+          </div>
+          <div>
+            <ul class="flexList">
+              <li>
+                <h2 class="mb-2 comicsTitle">SITES</h2>
+              </li>
+              <li class="comicsList">DC</li>
+              <li class="comicsList">MAD Magazine</li>
+              <li class="comicsList">DC Kids</li>
+              <li class="comicsList">DC Universe</li>
+              <li class="comicsList">DC Power Visa</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
