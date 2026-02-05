082c0b0a60005e2d20c6b419b239cdef0c818ed0
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-05 16:36:28 +0100

    bonus

--- src/assets/components/MainSection.jsx
+++ src/assets/components/MainSection.jsx
@@ -1,62 +1,150 @@
 export default function mainSection() {
+  const dcList = [
+    {
+      title1: "DC COMICS",
+      links1: [
+        {
+          text: "Characters",
+        },
+        {
+          text: "Comics",
+        },
+        {
+          text: "Movies",
+        },
+        {
+          text: "TV",
+        },
+        {
+          text: "Games",
+        },
+        {
+          text: "Videos",
+        },
+        {
+          text: "News",
+        },
+      ],
+      title2: "SHOP",
+      links2: [
+        {
+          text: "Shop DC",
+        },
+        {
+          text: "Shop DC Collectibles",
+        },
+      ],
+      title3: "DC",
+      links3: [
+        {
+          text: "Terms Of Use",
+        },
+        {
+          text: "Privacy policy(New)",
+        },
+        {
+          text: "Ad Choices",
+        },
+        {
+          text: "Advertising",
+        },
+        {
+          text: "Jobs",
+        },
+        {
+          text: "Subscription",
+        },
+        {
+          text: "Talent Workshop",
+        },
+        {
+          text: "CPSC Certificates",
+        },
+        {
+          text: "Ratings",
+        },
+        {
+          text: "Shop Help",
+        },
+        {
+          text: "Contact Us",
+        },
+      ],
+      title4: "SITES",
+      links4: [
+        {
+          text: "DC",
+        },
+        {
+          text: "MAD Magazine",
+        },
+        {
+          text: "DC Kids",
+        },
+        {
+          text: "DC Universe",
+        },
+        {
+          text: "DC Power Visa",
+        },
+      ],
+    },
+  ];
+
   return (
     <>
       <div class="listContainer ">
         <div class="container d-flex py-5 sndBackground">
           <div>
-            <ul class="flexList">
-              <li>
-                <h2 class="mb-2 comicsTitle" Shop DC>
-                  DC COMICS
-                </h2>
-              </li>
-              <li class="comicsList">Characters</li>
-              <li class="comicsList">Comics</li>
-              <li class="comicsList">Movies</li>
-              <li class="comicsList">TV</li>
-              <li class="comicsList">Games</li>
-              <li class="comicsList">Videos</li>
-              <li class="comicsList">News</li>
-            </ul>
-            <ul class="flexList">
-              <li>
-                <h2 class="mb-2 comicsTitle">SHOP</h2>
-              </li>
-              <li class="comicsList">Shop DC</li>
-              <li class="comicsList">Shop DC Collectibles</li>
-            </ul>
+            {dcList.map((element) => {
+              return (
+                <ul class="flexList">
+                  <h2 class="mb-2 comicsTitle">{element.title1}</h2>
+
+                  {element.links1.map((testo) => {
+                    return <li className="comicsList">{testo.text}</li>;
+                  })}
+                </ul>
+              );
+            })}
+
+            {dcList.map((element) => {
+              return (
+                <ul class="flexList">
+                  <h2 class="mb-2 comicsTitle">{element.title2}</h2>
+
+                  {element.links2.map((testo) => {
+                    return <li className="comicsList">{testo.text}</li>;
+                  })}
+                </ul>
+              );
+            })}
           </div>
           <div>
-            <ul class="flexList">
-              <li>
-                <h2 class="mb-2 comicsTitle" Shop DC>
-                  DC COMICS
-                </h2>
-              </li>
-              <li class="comicsList">Terms Of Use</li>
-              <li class="comicsList">Privacy policy(New)</li>
-              <li class="comicsList">Ad Choices </li>
-              <li class="comicsList">Advertising</li>
-              <li class="comicsList">Jobs</li>
-              <li class="comicsList">Subscription</li>
-              <li class="comicsList">Talent Workshop</li>
-              <li class="comicsList">CPSC Certificates</li>
-              <li class="comicsList">Ratings</li>
-              <li class="comicsList">Shop Help</li>
-              <li class="comicsList">Contact Us</li>
-            </ul>
+            {dcList.map((element) => {
+              return (
+                <ul class="flexList">
+                  <h2 class="mb-2 comicsTitle">{element.title3}</h2>
+
+                  {element.links3.map((testo) => {
+                    return <li className="comicsList">{testo.text}</li>;
+                  })}
+                </ul>
+              );
+            })}
           </div>
           <div>
-            <ul class="flexList">
-              <li>
-                <h2 class="mb-2 comicsTitle">SITES</h2>
-              </li>
-              <li class="comicsList">DC</li>
-              <li class="comicsList">MAD Magazine</li>
-              <li class="comicsList">DC Kids</li>
-              <li class="comicsList">DC Universe</li>
-              <li class="comicsList">DC Power Visa</li>
-            </ul>
+            {dcList.map((element) => {
+              return (
+                <ul class="flexList">
+                  <h2 class="mb-2 comicsTitle">{element.title4}</h2>
+
+                  {element.links4.map((testo) => {
+                    return <li className="comicsList">{testo.text}</li>;
+                  })}
+                </ul>
+              );
+            })}
           </div>
         </div>
       </div>
