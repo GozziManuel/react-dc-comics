d88478fd1d7eb0c5f811d2930721332c01d5c81b
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-06 21:55:14 +0100

    card problem

---
+++ src/assets/components/products/comicCard.jsx
@@ -0,0 +1,8 @@
+export default function ComicCard(thumb, series) {
+  return (
+    <div class="col-2 cards">
+      <img class="cardImg" src={thumb} alt="thumb" />
+      <p class="cardText">{series}</p>
+    </div>
+  );
+}
